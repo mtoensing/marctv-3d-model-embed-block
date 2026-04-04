e707ddf01d21cdf8ed2f173d3e16fea41833fd58
Marc Tönsing <marc.toensing@gmail.com>
2026-04-04 15:50:10 +0200

    Improve mobile viewer height

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9394a408312aca05591c');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'ac85a8976631f4d30a85');
